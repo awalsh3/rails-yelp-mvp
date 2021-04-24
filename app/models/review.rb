2418b2055d46a378e0470b182d5c00f227082c6a
Annie Walsh <annie@anyonething.co.uk>
2021-04-24 11:59:20 +0100

    review model complete

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: 0..5 }
   validates :content, presence: true
 
 end
